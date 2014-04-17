14da5b38dea0310692284ba9246d1cccc9d606f9
shridharan chandramouli <sdharan@cs.utah.edu>
2014-04-17 14:44:52 -0600

    ..

--- graph.cpp
+++ graph.cpp
@@ -16,20 +16,91 @@ void Graph::buildAdjacency(bool verbose) {
     if (verbose) {
         std::cout << "Building adjacency...\n";
     }
-    int offsets[2][2] = { {-1 ,0}, { 0,-1}, {0,1}, {1,0}};
+
+    this->m_capacity = new float[this->x*this->y];
+    memset(this->m_capacity,0, x*y);
+    this->m_flow = new float[x*y];
+    memset(this->m_flow, 0, x*y);
+
     for (int i = 0; i < x; i++) {
         for (int j = 0; j < y; j++) {
             for (int k = 0; k < 4; k++) {
                 int nx = i + offsets[k][0];
                 int ny = j + offsets[k][0];
                 if (nx >= 0 && nx < x && ny >=0 && ny < y) {
-                    this->m_adjweights[i*y+j + k] = this->m_pixelData[i*y+j] - this->m_pixelData[nx*y+ny];
+                    this->m_capacity[i*y+j + k] = this->m_pixelData[i*y+j] - this->m_pixelData[nx*y+ny];
                 }
                 else {
-                    this->m_adjweights[i*y+j+k] = 0;
+                    this->m_capacity[i*y+j+k] = 0;
                 }
             }
         }
+    }
+}
+
+float Graph::mincut(ALGORITHM type) {
+    switch (type) {
+    case SERIAL_FF:
+        return this->serialFF();
+    default:
+        std::cout << "Not implemented yet!";
+    }
+}
+
+float Graph::serialFF() {
+    float flow;
+    vec2 size(this->x,this->y);
+    vec2 start(0,0);
+    int pred[size.x*size.y];
+    bool visited[this->x][this->y];
+    for (int i = 0; i < this->x; i++) {
+        memset(visited, 0, this->x*this->y);
+        memset(pred, 0, this->x*this->y);
+        vec2 end;
+        while (findPath(start,&end, pred, visited)) {
+            for (u=n-1; pred[u]>=0; u=pred[u]) {
+                increment = min(increment,capacity[pred[u]][u]-flow[pred[u]][u]);
+            }
+            // Now increment the flow.
+            for (u=n-1; pred[u]>=0; u=pred[u]) {
+                flow[pred[u]][u] += increment;
+                flow[u][pred[u]] -= increment;
+            }
+            max_flow += increment;
+        }
+        start.x++;
+    }
+}
+
+bool Graph::findPath(vec2 start, vec2 *end, int **pred, bool **visited, vec2 size) {
+    int head = 0, tail = 0;
+
+    int queue[size.x * size.y];
+    queue[tail++] = start.sub2ind(size);
+    visited[start.x][start.y] = true;
+    bool pathfound = false;
+    pred[start.sub2ind(size)] = -1;
+    vec2 end_node;
+    while (head != tail) {
+        int pos = queue[head++];
+
+        while (true) {
+            vec2 next_node = this->getUnvisitedNeighbor(size, visited, pos);
+            if (next_node.x == -1) {
+                break;
+            }
+            queue[tail++] = next_node.sub2ind(size);
+            visited[next_node.x][next_node.y] = true;
+            pred[next_node.sub2ind(size)] = pos;
+            if (next_node.isSink(size)) {
+                pathfound = true;
+                end_node = next_node;
+                break;
+            }
+        }
+    }
+    if (pathfound) {
+        end = &end_node;
     }
 }
 
--- graph.h
+++ graph.h
@@ -6,6 +6,16 @@
 #define L 2
 #define R 3
 namespace mincut {
+enum ALGORITHM {
+    SERIAL_FF,
+    GPU_FF,
+    SERIAL_BK,
+    SERIAL_PR,
+    GPU_PR,
+};
+int offsets[2][2] = { {-1 ,0}, { 0,-1}, {0,1}, {1,0}};
+
+
 class Graph
 {
 public:
@@ -14,12 +24,48 @@ public:
     unsigned int x,y,z;
 
     Graph(int x, int y, float **values);
-    float *m_adjweights;
+    float *m_capacity;
+    float *m_flow;
 
     void buildAdjacency(bool verbose = false);
 
+    float mincut(ALGORITHM type);
+    float serialFF();
+    inline vec2 getUnvisitedNeighbor(vec2 size, bool *visited, int pos) {
+        int xpos = pos / size.y;
+        int ypos = pos % size.y;
+
+        for (int i = 0; i < 4; i++) {
+            int xnext = xpos + offsets[i][0];
+            int ynext = ypos + offsets[i][1];
+            if (!visited[xnext*size.y + ynext] && (m_capacity[xnext][ynext] - m_flow[xnext][ynext] > 0)) {
+                return vec3(xnext,ynext);
+            }
+        }
+        return vec3(-1,-1);
+    }
+
 };
 
+class vec2
+{
+public:
+    vec2::vec2(int x, int y) : x(x), y(y) {}
+    int x,y;
+    bool isSink(vec2 size) {
+        if (y == size.y) {
+            return true;
+        }
+        return false;
+    }
+    inline int sub2ind(vec2 size){
+        return x*size.y + y;
+    }
+    static inline vec2 ind2sub(vec2 size, int pos) {
+        vec2 vecpos(pos/size.y, pos % size.y);
+        return vecpos;
+    }
+};
 }
 
 #endif // GRAPH_H
